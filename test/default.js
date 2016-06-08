ce934d9dbb6c9a134e31915399484d4d07cf5032
Michael van der Weg <michael@joinbox.com>
2016-06-08 16:11:29 +0200

    updated dependencies, removed node < 4 support

--- test/default.js
+++ test/default.js
@@ -1,41 +1,41 @@
-
-	
-	var   Class 		= require('ee-class')
-		, log 			= require('ee-log')
-		, assert 		= require('assert');
+(function() {
+    'use strict';
 
 
+    const log = require('ee-log');
+    const assert = require('assert');
+    const OMBDAPI = require('../');
 
-	var OMBDAPI = require('../')
-		, omdb;
-
-
-
-	describe('The OMBDAPI', function() {
-		it('should not crash when instantiated', function() {
-			omdb = new OMBDAPI();
-		});
-		
-
-
-		it('should be able to list movies', function(done) {
-			this.timeout(10000);
-
-			omdb({t:'chappie'}).list().then(function(movie) {
-				assert(movie);
-				done();
-			}).catch(done);
-		});
+    let omdb;
 
 
 
-		it('should be able to identify errors', function(done) {
-			this.timeout(10000);
+    describe('The OMBDAPI', function() {
+        it('should not crash when instantiated', function() {
+            omdb = new OMBDAPI();
+        });
+        
 
-			omdb({t:'chappipe'}).list().then(function(movie) {}).catch(function(err) {
-				assert(err instanceof Error);
-				done();
-			});
-		});
-	});
-	
+
+        it('should be able to list movies', function(done) {
+            this.timeout(10000);
+
+            omdb({t:'chappie'}).list().then((movie) => { log(movie);
+                assert(movie);
+                done();
+            }).catch(done);
+        });
+
+
+
+        it('should be able to identify errors', function(done) {
+            this.timeout(10000);
+
+            omdb({t:'chappipe'}).list().then(() => {}).catch((err) => {
+                assert(err instanceof Error);
+                done();
+            });
+        });
+    });
+})();   
+    
